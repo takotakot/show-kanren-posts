58208f4d6308998df411d690f789b5a61a88e3b5
takotakot <takotakot@users.noreply.github.com>
2023-04-15 20:29:18 +0900

    feat: Add backslashes

--- tests/unit/ShowKanrenPostsTest.php
+++ tests/unit/ShowKanrenPostsTest.php
@@ -8,14 +8,14 @@ namespace Tests;
  */
 class ShowKanrenPostsTest extends TestCase
 {
-    protected Show_Kanren_Posts $show_kanren_posts;
+    protected \Show_Kanren_Posts $show_kanren_posts;
 
     public function setUp() :void{
-        $this->show_kanren_posts = new Show_Kanren_Posts();
+        $this->show_kanren_posts = new \Show_Kanren_Posts();
     }
 
     public function testInstanceOf() {
-        $this->assertInstanceOf(Show_Kanren_Posts::class, $this->show_kanren_posts);
+        $this->assertInstanceOf(\Show_Kanren_Posts::class, $this->show_kanren_posts);
     }
 
     /** @test */
